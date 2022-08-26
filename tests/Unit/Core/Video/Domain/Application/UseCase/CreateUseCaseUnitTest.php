ddab49b74276ac9445147467255ba3ab14f89294
LeandroSousaCastro <leandro.desousacastro@gmail.com>
2022-08-26 16:14:50 +0000

    test(Use Case): :white_check_mark: Implementa testes de upload de arquivos.

--- tests/Unit/Core/Video/Domain/Application/UseCase/CreateUseCaseUnitTest.php
+++ tests/Unit/Core/Video/Domain/Application/UseCase/CreateUseCaseUnitTest.php
@@ -25,25 +25,40 @@ use stdClass;
 
 class CreateUseCaseUnitTest extends TestCase
 {
-    protected $useCase;
+    protected  $useCase;
 
-    protected function setUp(): void
-    {
+    protected function createUseCase(
+        int $timesCallMethodActionRepository = 1,
+        int $timesCallMethodUpdateMediaRepository = 1,
+        int $timesCallMethodCommitTransaction = 1,
+        int $timesCallMethodRollbackTransaction = 0,
+        int $timesCallMethodStoreFileStorage = 0,
+        int $timesCallMethodDispatchEventManager = 0,
+    ) {
         $this->useCase = new CreateUseCase(
-            repository: $this->createMockRepository(),
+            repository: $this->createMockRepository(
+                timesCallAction: $timesCallMethodActionRepository,
+                timesCallUpdateMedia: $timesCallMethodUpdateMediaRepository
+            ),
             categoryRepository: $this->createMockRepositoryCategory(),
             genreRepository: $this->createMockRepositoryGenre(),
             castMembersRepository: $this->createMockRepositoryCastMembers(),
-            transaction: $this->createMockTransaction(),
-            storage: $this->createMockFileStorage(),
-            eventManager: $this->createMockEventManager(),
+            transaction: $this->createMockTransaction(
+                timesCallCommit: $timesCallMethodCommitTransaction,
+                timesCallRollback: $timesCallMethodRollbackTransaction
+            ),
+            storage: $this->createMockFileStorage(
+                timesCall: $timesCallMethodStoreFileStorage
+            ),
+            eventManager: $this->createMockEventManager(
+                timesCall: $timesCallMethodDispatchEventManager
+            ),
         );
-
-        parent::setUp();
     }
 
     public function testExecuteInputOutput()
     {
+        $this->createUseCase();
         $response = $this->useCase->execute(
             input: $this->createMockInputDto()
         );
@@ -54,54 +69,122 @@ class CreateUseCaseUnitTest extends TestCase
      * @dataProvider dataProviderIds
      */
     public function testValidateCategoriesIds(
-        string $label,
-        array $ids,
+        array $arrange,
     ) {
-        $this->expectException(NotFoundException::class);
-        $this->expectErrorMessage(sprintf(
-            '%s %s not found',
-            $label,
-            implode(', ', $ids)
-        ));
-        $this->useCase->execute(
-            input: $this->createMockInputDto(
-                categoriesIds: $ids
-            )
+        $this->createUseCase(
+            timesCallMethodActionRepository: 0,
+            timesCallMethodUpdateMediaRepository: 0,
+            timesCallMethodCommitTransaction: 0,
         );
+
+        foreach ($arrange as $data) {
+            $this->expectException(NotFoundException::class);
+            $this->expectErrorMessage(sprintf(
+                '%s %s not found',
+                $data[0],
+                implode(', ', $data[1])
+            ));
+            $this->useCase->execute(
+                input: $this->createMockInputDto(
+                    categoriesIds: $data[1]
+                )
+            );
+        }
     }
 
     public function dataProviderIds(): array
     {
         return [
-            ['Category', ['uuid-1']],
-            ['Categories', ['uuid-1', 'uuid-2']],
-            ['Categories', ['uuid-1', 'uuid-2', 'uuid-3', 'uuid-4']],
+            'arrange' => [[
+                ['Category', ['uuid-1']],
+                ['Categories', ['uuid-1', 'uuid-2']],
+                ['Categories', ['uuid-1', 'uuid-2', 'uuid-3', 'uuid-4']],
+                ['Genre', ['uuid-1']],
+                ['Genres', ['uuid-1', 'uuid-2']],
+                ['Genres', ['uuid-1', 'uuid-2', 'uuid-3', 'uuid-4']],
+                ['CastMember', ['uuid-1']],
+                ['CastMembers', ['uuid-1', 'uuid-2']],
+                ['CastMembers', ['uuid-1', 'uuid-2', 'uuid-3', 'uuid-4']],
+            ]]
         ];
     }
 
-    public function testUploadFiles()
-    {
+    /**
+     * @dataProvider dataProviderFiles
+     */
+    public function testUploadFiles(
+        array $thumb,
+        array $thumbHalf,
+        array $banner,
+        array $video,
+        array $trailer,
+        int $timesStorageCalls = 0,
+        int $timesDispatchCalls = 0,
+    ) {
+        $this->createUseCase(
+            timesCallMethodStoreFileStorage: $timesStorageCalls,
+            timesCallMethodDispatchEventManager: $timesDispatchCalls
+        );
+
         $response = $this->useCase->execute(
             input: $this->createMockInputDto(
-                thumbFile: ['tmp' => 'tmp/thumbFile.png'],
-                thumbHalf: ['tmp' => 'tmp/thumbHalf.png'],
-                bannerFile: ['tmp' => 'tmp/bannerFile.png'],
-                trailerFile: ['tmp' => 'tmp/trailerFile.mp4'],
-                videoFile: ['tmp' => 'tmp/videoFile.mp4']
+                thumbFile: $thumb['value'],
+                thumbHalf: $thumbHalf['value'],
+                bannerFile: $banner['value'],
+                trailerFile: $video['value'],
+                videoFile: $trailer['value'],
             )
         );
-        $this->assertNotNull($response->thumbFile);
-        $this->assertNotNull($response->thumbHalf);
-        $this->assertNotNull($response->bannerFile);
-        $this->assertNotNull($response->trailerFile);
-        $this->assertNotNull($response->videoFile);
+        $this->assertEquals($response->thumbFile, $thumb['expected']);
+        $this->assertEquals($response->thumbHalf, $thumbHalf['expected']);
+        $this->assertEquals($response->bannerFile, $banner['expected']);
+        $this->assertEquals($response->trailerFile, $video['expected']);
+        $this->assertEquals($response->videoFile, $trailer['expected']);
     }
 
-    private function createMockRepository()
+    public function dataProviderFiles(): array
     {
+        $expected = 'path/file.png';
+        return [
+            [
+                'thumb' => ['value' => ['tmp' => 'tmp/thumb.png'], 'expected' => $expected],
+                'thumbHalf' => ['value' => ['tmp' => 'tmp/thumbHalf.png'], 'expected' => $expected],
+                'banner' => ['value' => ['tmp' => 'tmp/banner.png'], 'expected' => $expected],
+                'trailer' => ['value' => ['tmp' => 'tmp/trailer.mp4'], 'expected' => $expected],
+                'video' => ['value' => ['tmp' => 'tmp/video.mp4'], 'expected' => $expected],
+                'timesStorageCalls' => 5,
+                'timesDispatchCalls' => 1
+            ], [
+                'thumb' => ['value' => null, 'expected' => null],
+                'thumbHalf' => ['value' => ['tmp' => 'tmp/thumbHalf.png'], 'expected' => $expected],
+                'banner' => ['value' => null, 'expected' => null],
+                'trailer' => ['value' => ['tmp' => 'tmp/trailer.mp4'], 'expected' => $expected],
+                'video' => ['value' => null, 'expected' => null],
+                'timesStorageCalls' => 2,
+            ], [
+                'thumb' => ['value' => ['tmp' => 'tmp/thumb.png'], 'expected' => $expected],
+                'thumbHalf' => ['value' => ['tmp' => 'tmp/thumbHalf.png'], 'expected' => $expected],
+                'banner' => ['value' => null, 'expected' => null],
+                'trailer' => ['value' => ['tmp' => 'tmp/trailer.mp4'], 'expected' => $expected],
+                'video' => ['value' => null, 'expected' => null],
+                'timesStorageCalls' => 3,
+            ], [
+                'thumb' => ['value' => null, 'expected' => null],
+                'thumbHalf' => ['value' => null, 'expected' => null],
+                'banner' => ['value' => null, 'expected' => null],
+                'trailer' => ['value' => null, 'expected' => null],
+                'video' => ['value' => null, 'expected' => null],
+            ]
+        ];
+    }
+
+    private function createMockRepository(
+        int $timesCallAction,
+        int $timesCallUpdateMedia
+    ) {
         $mockRepository = Mockery::mock(stdClass::class, VideoRepositoryInterface::class);
-        $mockRepository->shouldReceive('insert')->andReturn($this->createMockEntity());
-        $mockRepository->shouldReceive('updateMedia');
+        $mockRepository->shouldReceive('insert')->times($timesCallAction)->andReturn($this->createMockEntity());
+        $mockRepository->shouldReceive('updateMedia')->times($timesCallUpdateMedia);
         return $mockRepository;
     }
 
@@ -126,25 +209,27 @@ class CreateUseCaseUnitTest extends TestCase
         return $mockRepository;
     }
 
-    private function createMockTransaction()
-    {
+    private function createMockTransaction(
+        int $timesCallCommit,
+        int $timesCallRollback,
+    ) {
         $mockTransaction = Mockery::mock(stdClass::class, DbTransactionInterface::class);
-        $mockTransaction->shouldReceive('commit');
-        $mockTransaction->shouldReceive('rollback');
+        $mockTransaction->shouldReceive('commit')->times($timesCallCommit);
+        $mockTransaction->shouldReceive('rollback')->times($timesCallRollback);
         return $mockTransaction;
     }
 
-    private function createMockFileStorage()
+    private function createMockFileStorage(int $timesCall)
     {
         $mockFileStorage = Mockery::mock(stdClass::class, FileStorageInterface::class);
-        $mockFileStorage->shouldReceive('store')->andReturn('path/file.png');
+        $mockFileStorage->shouldReceive('store')->times($timesCall)->andReturn('path/file.png');
         return $mockFileStorage;
     }
 
-    private function createMockEventManager()
+    private function createMockEventManager(int $timesCall)
     {
         $mockEventManager = Mockery::mock(stdClass::class, VideoEventManagerInterface::class);
-        $mockEventManager->shouldReceive('dispatch');
+        $mockEventManager->shouldReceive('dispatch')->times($timesCall);
         return $mockEventManager;
     }
 
